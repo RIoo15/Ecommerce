f2a4e802d9f8a1709a9200ba1650853023e5c158
RIoo15 <hashim74100@gmail.com>
2023-11-21 11:10:01 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import CategoryPage from "./pages/CategoryPage";
+import ProductPage from "./pages/Productpage";
+import Cart from "./pages/Cart";
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+   
+   <Home/>
+    </div>
+  );
+}
+
+export default App;
